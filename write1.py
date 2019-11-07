695c38ac79600475297d4a600c8cf3a2c8d12022
Santosh Krishnan R <46351924+santoshkrishnanr@users.noreply.github.com>
2019-11-07 13:41:35 +0100

    Add files via upload

---
+++ write1.py
@@ -0,0 +1,12 @@
+
+import time
+import datetime
+c=1          
+while 1:
+	        
+	print (c)
+	c+=1
+	#ser.write(a)        
+        #print("sent on",now.strftime("%H:%M:%S:%f"))
+	time.sleep(1)
+        
